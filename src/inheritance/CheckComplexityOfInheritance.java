018bdf1880971a8d46d0a85976e3165b3d7ea422
harini hapuarachchi <hariniupendrika@gmail.com>
2019-08-09 23:49:48 +0530

    add complexity of inheritance

---
+++ src/inheritance/CheckComplexityOfInheritance.java
@@ -0,0 +1,61 @@
+package inheritance;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+public class CheckComplexityOfInheritance {
+
+	static String p = null;
+	
+	public static void main(String[] args) throws IOException {
+		 p = "/home/harsha/Music/inheritancePro/CustomerDTO.java";
+
+	        int superClass = 0;
+
+	        while (p != null) {
+	            File f = new File(p);
+	            Scanner input = new Scanner(f);
+
+	            int count = 0;
+	            while (input.hasNext()) {
+	                String word = input.next();
+	                System.out.println(word);
+
+	                if (word.equals("extends")) {
+	                    superClass += 1;
+
+	                    while (input.hasNext()) {
+	                        String lWord = input.next();
+
+	                        System.out.println(lWord);
+	                        String[] className = lWord.split("[{]");
+
+	                        System.out.println(className[0]);
+	                        Path path = Paths.get(f.getAbsolutePath());
+	                        String directory = path.getParent().toString();
+
+	                        p = directory + "/" + className[0] + ".java";
+	                        System.out.println(p);
+
+	                        break;
+
+	                    }
+
+	                    break;
+	                }else {
+	                    p = null;
+	                }
+
+	            }
+
+	        }
+	        System.out.println(superClass);
+	        int com = superClass+1;
+	        System.out.println("Complexity: "+com);
+	    }
+	
+
+}
